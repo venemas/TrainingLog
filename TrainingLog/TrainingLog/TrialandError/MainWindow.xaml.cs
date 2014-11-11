7498790c36a484377733efe90ed8869e09b15ae0
Sjoerd Venema <venemas@hotmail.com>
2014-11-11 22:01:02 +0100

    opruimactie

---
+++ TrainingLog/TrainingLog/TrialandError/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace TrainingLog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test!");
+        }
+
+        private void Header_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        
+        private void windowSjoerd_Click(object sender, RoutedEventArgs e)
+        {
+            Sjoerd wnd = new Sjoerd();
+            wnd.Show();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void windowSander_Click(object sender, RoutedEventArgs e)
+        {
+            Sander wnd = new Sander();
+            wnd.Show();
+        }
+    }
+}
