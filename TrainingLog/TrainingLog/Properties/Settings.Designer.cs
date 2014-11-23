73d70f49c11be2fb8ea3153b0844cf8470e02e8a
BikerMouse <s.vanbueren@gmail.com>
2014-11-23 12:13:46 +0100

    Clean up

--- TrainingLog/TrainingLog/Properties/Settings.Designer.cs
+++ TrainingLog/TrainingLog/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TrainingLog.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,17 +20,6 @@ namespace TrainingLog.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\DummyData.mdf;Integr" +
-            "ated Security=True")]
-        public string DummyDataConnectionString {
-            get {
-                return ((string)(this["DummyDataConnectionString"]));
             }
         }
     }
