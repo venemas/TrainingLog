dee6fc56e8cd2c6bf1f7694f67a119ada2291786
Sjoerd Venema <venemas@hotmail.com>
2014-11-23 10:14:44 +0100

    velden in new user met datumcontrole

--- TrainingLog/TrainingLog/NewUser.xaml.cs
+++ TrainingLog/TrainingLog/NewUser.xaml.cs
@@ -31,12 +31,69 @@ namespace TrainingLog
 
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                DateTime dte = new DateTime(Convert.ToInt32(birthdayYear.Text), Convert.ToInt32(birthdayMonth.Text), Convert.ToInt32(birthdayDay.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid date of birth");
+            }
         }
 
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+                
+        private void userNameBox_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            userNameBox.SelectAll();
+        }
+
+        private void birthdayDay_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            birthdayDay.SelectAll();
+        }
+
+        private void birthdayMonth_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            birthdayMonth.SelectAll();
+        }
+
+        private void birthdayYear_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            birthdayYear.SelectAll();
+        }
+
+        private void lenghthBox_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            lenghthBox.SelectAll();
+        }
+        
+        private void birthdayDay_GotFocus(object sender, RoutedEventArgs e)
+        {
+            birthdayDay.SelectAll();
+        }
+
+        private void birthdayMonth_GotFocus(object sender, RoutedEventArgs e)
+        {
+            birthdayMonth.SelectAll();
+        }
+
+        private void birthdayYear_GotFocus(object sender, RoutedEventArgs e)
+        {
+            birthdayYear.SelectAll();
+        }
+
+        private void userNameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            userNameBox.SelectAll();
+        }
+
+        private void lenghthBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            lenghthBox.SelectAll();
         }
     }
 }
